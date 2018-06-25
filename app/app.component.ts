ff0279231d30b8cc10ab357f1b8d11d2ebd031b1
Brad Le <brad.le444@gmail.com>
2018-06-24 22:19:43 -0700

    services

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,27 +1,22 @@
 import { Component } from "@angular/core";
+import { User } from "./shared/user/user";
+import {UserService} from "./shared/user/user.service";
 
 @Component({
   selector: "my-app",
-  template: `
-  <StackLayout>
-    <Image src="res://logo_login" stretch="none" horizontalAlignment="center"></Image>
-    <TextField [(ngModel)]="email" hint="Email Address" keyboardType="email"
-      autocorrect="false" autocapitalizationType="none"></TextField>
-    <TextField hint="Password" secure="true"></TextField>
-
-    <Button [text]="isLoggingIn ? 'Sign in' : 'Sign up'" class="submit-button" (tap)="submit()"></Button>
-    <Button [text]="isLoggingIn ? 'Sign up' : 'Back to login'" (tap)="toggleDisplay()"></Button>
-  
-  </StackLayout>
-  `,
+  templateUrl: 'pages/login/login.html',
   styleUrls: ['pages/login/login-common.css','pages/login/login.css']
 })
 export class AppComponent {
-  email='yourEmail@something.com';
+  user: User;
   isLoggingIn = true;
 
+  constructor() {
+    this.user = new User();
+
+  }
   submit() {
-    alert('You\'re using: ' + this.email);
+    alert('You\'re using: ' + this.user.email);
   }
   toggleDisplay() {
     this.isLoggingIn = !this.isLoggingIn;
